b85793db9892c90fa707f41dcd0952a34f6b35e0
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-12-14 23:15:05 -0300

    Task: Route delete todo

--- src/index.js
+++ src/index.js
@@ -9,21 +9,21 @@ const users = [
     username: "leeodesign",
     todos: [
       {
-        id: "f9dd42db-99cf-46f7-84a3-f86a2f40016f",
+        id: "f925dd42db-99cf-46f7-84a3-e5f1a2f40016f",
         title: "Terminar API Todos",
         done: false,
         deadline: "2021-12-19T00:00:00.000Z",
         created_at: "2021-12-15T01:49:49.078Z",
       },
       {
-        id: "f9dd42db-99cf-46f7-84a3-f86a2f40016f",
+        id: "f9d25d42db-99cf-46f7-84a3-w65ef14f40016f",
         title: "Buy new Shoes",
         done: true,
         deadline: "2021-12-19T00:00:00.000Z",
         created_at: "2021-12-15T01:49:49.078Z",
       },
       {
-        id: "f9dd42db-99cf-46f7-84a3-f86a2f40016f",
+        id: "f9dd42785db-99cf-46f7-84a3-f86a2f40016f",
         title: "Shopping with friends",
         done: true,
         deadline: "2021-12-19T00:00:00.000Z",
@@ -87,7 +87,6 @@ app.delete("/users/:id", (request, response) => {
   try {
     const { id } = request.params;
     const userIndex = users.findIndex((user) => user.id === id);
-    console.log(userIndex);
     if (userIndex < 0) throw new Error("User not found or id incorrect!");
     users.splice(userIndex, 1);
     return response.status(204).json();
@@ -143,7 +142,6 @@ app.get("/todos", checkExistsUserAccount, (request, response) => {
   const done = user.todos.filter((todo) => {
     if (todo.done) return todo;
   });
-  console.log(done);
   const remain = user.todos.filter((todo) => {
     if (!todo.done) return todo;
   });
@@ -155,6 +153,21 @@ app.get("/todos", checkExistsUserAccount, (request, response) => {
     remain,
     remain_total: remain.length,
   });
+});
+app.delete("/todos/:id", checkExistsUserAccount, (request, response) => {
+  try {
+    const { user } = request;
+    const { id } = request.params;
+    const todo = user.todos.find((todo) => todo.id === id);
+    if (!todo) throw new Error("Todo not found!");
+    const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+    console.log(todoIndex);
+    user.todos.splice(todoIndex, 1);
+
+    return response.status(204).json();
+  } catch (error) {
+    return response.status(404).json({ error: error.message });
+  }
 });
 app.get("/todos/:id", checkExistsUserAccount, (request, response) => {
   try {
